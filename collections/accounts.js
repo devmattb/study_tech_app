91f7544d533bd5bbf94cd409c3c417e8d81c4bc7
Matt Bergstrom <matt_bergstrom@icloud.com>
2018-03-18 17:37:18 +0100

    deployable

--- collections/accounts.js
+++ collections/accounts.js
@@ -8,83 +8,83 @@
 *
 ***/
 
-import {Mongo} from 'meteor/mongo';
-import SimpleSchema from 'simpl-schema';
-
-Accounts = new Mongo.Collection('Accounts');
-
-AccountsSchema = new SimpleSchema({
-
-  user: {
-    type: String,
-    label:"user",
-    optional: true
-  },
-
-  pass: {
-    type: String,
-    label:"pass",
-    optional: true
-  },
-
-  email: {
-    type: String,
-    label:"email",
-    optional: true
-  },
-
-  agreedEmailList: {
-    type: Boolean,
-    label:"agreedEmailList",
-    optional: true
-  },
-
-  agreedTermsOfUse: {
-    type: Boolean,
-    label:"agreedTermsOfUse",
-    optional: true,
-    defaultValue: true
-  },
-
-  //TODO
-  themes: {
-    type: Array,
-    label:"themes",
-  },
-
-  lang: {
-    type: String,
-    label:"lang",
-    optional: true,
-    defaultValue: "SWE"
-  },
-
-/*
-  TODO
-  courses: {
-    type: String,
-    label:"theme",
-    optional: true,
-  },
-
-  */
-
-/*  step: {
-    type: SimpleSchema.Integer,
-    label:"step",
-    optional: true,
-    min: 1,
-    max: 5,
-  },*/
-
-  lastActive: {
-      type: Date,
-      label: "lastActive",
-      autoValue: function() {
-          return new Date();
-      }
-  }
-
-});
-
-Accounts.attachSchema(AccountsSchema);
+// import {Mongo} from 'meteor/mongo';
+// import SimpleSchema from 'simpl-schema';
+//
+// Accounts = new Mongo.Collection('Accounts');
+//
+// AccountsSchema = new SimpleSchema({
+//
+//   user: {
+//     type: String,
+//     label:"user",
+//     optional: true
+//   },
+//
+//   pass: {
+//     type: String,
+//     label:"pass",
+//     optional: true
+//   },
+//
+//   email: {
+//     type: String,
+//     label:"email",
+//     optional: true
+//   },
+//
+//   agreedEmailList: {
+//     type: Boolean,
+//     label:"agreedEmailList",
+//     optional: true
+//   },
+//
+//   agreedTermsOfUse: {
+//     type: Boolean,
+//     label:"agreedTermsOfUse",
+//     optional: true,
+//     defaultValue: true
+//   },
+//
+//   //TODO
+//   themes: {
+//     type: Array,
+//     label:"themes",
+//   },
+//
+//   lang: {
+//     type: String,
+//     label:"lang",
+//     optional: true,
+//     defaultValue: "SWE"
+//   },
+//
+// /*
+//   TODO
+//   courses: {
+//     type: String,
+//     label:"theme",
+//     optional: true,
+//   },
+//
+//   */
+//
+// /*  step: {
+//     type: SimpleSchema.Integer,
+//     label:"step",
+//     optional: true,
+//     min: 1,
+//     max: 5,
+//   },*/
+//
+//   lastActive: {
+//       type: Date,
+//       label: "lastActive",
+//       autoValue: function() {
+//           return new Date();
+//       }
+//   }
+//
+// });
+//
+// Accounts.attachSchema(AccountsSchema);
