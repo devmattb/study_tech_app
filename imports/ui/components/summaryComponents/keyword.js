0f3b1fd343ac48b49690e5fedcff0996d81fa139
Matt Bergstrom <matt_bergstrom@icloud.com>
2018-07-23 17:20:11 +0200

    some basics working

--- imports/ui/components/summaryComponents/keyword.js
+++ imports/ui/components/summaryComponents/keyword.js
@@ -1,1 +1,5 @@
 import "./keyword.html";
+
+Template.keyword.onRendered(function(){
+  Meteor.defer(function(){$(".keywordContainer").removeClass("scale-out");},500);
+});
--- imports/ui/pages/summaryPage.js
+++ imports/ui/pages/summaryPage.js
@@ -4,9 +4,34 @@ import "../components/preloader.js";
 import "../components/summaryComponents/keyword.js";
 import "./summaryPage.html";
 import "../components/modals/keywordModal.js";
+import "../components/summaryComponents/keyword.js";
 
 import {pageInit} from "../../api/functions/pageInit"
 import {subscriptions} from "../../api/functions/subscriptions"
+
+function addKeyword() {
+  // Get the current "keywords" json object.
+  var keywords = Session.get("keywords");
+  // Add to the current "keywords" json object.
+  Session.set("numKeywords", Session.get("numKeywords")+1);
+  keywords["keys"][Session.get("numKeywords")] = {
+    index: Session.get("numKeywords"),
+    keywordValue: "",
+    keywordDescription: "",
+  }
+  // Save the new "keywords" json object in the session variable.
+  Session.set("keywords", keywords);
+}
+
+function removeKeyword() {
+  // Get the current "keywords" json object.
+  var keywords = Session.get("keywords");
+  // Delete latest keyword from the current "keywords" json object.
+  Session.set("numKeywords", Session.get("numKeywords")-1);
+  delete keywords["keys"][Session.get("numKeywords")];
+  // Save the new "keywords" json object in the session variable.
+  Session.set("keywords", keywords);
+}
 
 
 Template.summaryPage.onCreated( () => {
@@ -16,26 +41,42 @@ Template.summaryPage.onCreated( () => {
 
 Template.summaryPage.onRendered(function(){
   pageInit();
+  // Init number of displayed keywords
+  if (!Session.get("numKeywords")) {
+    Session.set("numKeywords", 1);
+    Session.set("keywords", {"keys":[]});
+  }
+});
+
+Template.summaryPage.helpers({
+
+  // Keeps track of the number of keywords.
+  numKeywords: function(){
+    return Session.get("numKeywords");
+  },
+
+  keywords: function(){
+    return Session.get("keywords");
+  },
+
 });
 
 var keywordCounter = 0;
 Template.summaryPage.events({
 
   "click #addKeywordContainerBtn":function(event) {
-    // Slide in
-    keywordCounter++;
-    var keywordHtml = `
-      <div class="keywordContainer red col s12 white-text">
-        <span id="keywordTxt" class="left">TEST`+keywordCounter+`</span>
-        <span class="right deleteKeywordContainer"><i style="font-size: 16px;" class="fa fa-times"></i></span>
-      </div>`;
-    $("#keywordBox").append(keywordHtml).fadeIn(900);
+    addKeyword();
+    // keywordCounter++;
+    // var keywordHtml = `
+    //   <div class="keywordContainer red col s12 white-text">
+    //     <span id="keywordTxt" class="left">TEST`+keywordCounter+`</span>
+    //     <span class="right deleteKeywordContainer"><i style="font-size: 16px;" class="fa fa-times"></i></span>
+    //   </div>`;
+    // $("#keywordBox").append(keywordHtml)
   },
 
   "click .deleteKeywordContainer":function(event) {
-    // Slide out
-    keywordCounter--;
-    $(event.target).closest(".keywordContainer").remove();
+    removeKeyword();
   },
 
 });
