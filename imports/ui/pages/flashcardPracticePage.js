395cecbef6e6660e363e7baeedd67581e1b01d95
marcuspousette <marcuspousette@gmail.com>
2018-09-20 18:14:49 +0200

    lägger in 21 svar ist för 20 i arrayOfAnsers. Fix it Jesus

--- imports/ui/pages/flashcardPracticePage.js
+++ imports/ui/pages/flashcardPracticePage.js
@@ -3,16 +3,6 @@ import "./flashcardPracticePage.html";
 import {pageInit} from "../../api/functions/pageInit";
 import {subscriptions} from "../../api/functions/subscriptions";
 
-
-var currentIndex = 0;
-var testArray = [
-  {keywordValueAnswer: "Cake", keywordDescriptionAnswer: "A bakery dish made with flour", connectedKeywordHashCode: "1"},
-  {keywordValueAnswer: "Ice cream", keywordDescriptionAnswer: "A mix of cream, sugar, milk and other goodies", connectedKeywordHashCode: "2"},
-  {keywordValueAnswer: "Ribs", keywordDescriptionAnswer: "The ribcage of a pig or cow, often BBQ’ed", connectedKeywordHashCode: "3"},
-  {keywordValueAnswer: "Mac and cheese", keywordDescriptionAnswer: "Cheese with macaroni, served warm", connectedKeywordHashCode: "4"},
-  {keywordValueAnswer: "Coca cola", keywordDescriptionAnswer: "The world's most famous soft drink", connectedKeywordHashCode: "5"}
-];
-
 var counter = 0;
 var amountOfReveals = 10; // TODO function to get amountOfReveals
 function allowToRevealAnswer(){
@@ -25,15 +15,110 @@ function allowToRevealAnswer(){
   }
 }
 
+var arrayOfAnswers = [];
+
+/*iterat over the array 4 times so the users practices 2 times for keywordValue and 2 times for keywordDescription
+*********************************/
+var contentIterations = 0; //shows how many times the array has been iterated
+var currentIndex = 0;
+var testArray = [
+  {keywordValue: "Cake", keywordDescription: "A bakery dish made with flour", connectedKeywordHashCode: "1"},
+  {keywordValue: "Ice cream", keywordDescription: "A mix of cream, sugar, milk and other goodies", connectedKeywordHashCode: "2"},
+  {keywordValue: "Ribs", keywordDescription: "The ribcage of a pig or cow, often BBQ’ed", connectedKeywordHashCode: "3"},
+  {keywordValue: "Mac and cheese", keywordDescription: "Cheese with macaroni, served warm", connectedKeywordHashCode: "4"},
+  {keywordValue: "Coca cola", keywordDescription: "The world's most famous soft drink", connectedKeywordHashCode: "5"}
+];
+function getMaxArrayIndex(){
+  return testArray.length - 1;
+}
+function setArrayIteration(){
+  if(currentIndex == getMaxArrayIndex()){
+    currentIndex = -1;
+    contentIterations += 1;
+  }
+}
+function checkIfContentHasBeenShowedFourTimes(){
+  if(contentIterations == 4){
+    //break the session here, the user is done
+    alert("you are done");
+    correctAnswers();
+  }else{
+    setArrayIteration();
+  }
+}
+
+// TODO
+function correctAnswers(){
+  length = arrayOfAnswers.length;
+  for(var i=0; i < length; i++){
+    var stringToBeCorrected = arrayOfAnswers[i].answer;
+    var isDescAnswer = arrayOfAnswers[i].isADescriptionAnswer;
+    if(isDescAnswer){
+      correctDescription(stringToBeCorrected);
+    }else if(!isDescAnswer){
+      correctKeywordValue(stringToBeCorrected);
+    }
+  }
+  console.log(arrayOfAnswers);
+}
+
+function correctDescription(stringToBeCorrected){
+  console.log("correctDescription " + stringToBeCorrected);
+}
+
+function correctKeywordValue(stringToBeCorrected){
+  console.log("correctKeywordValue " + stringToBeCorrected);
+}
+
+
+function createAnswerObject(){
+  var usersAnswer = $("#textarea1").val();
+  var keywordHashCode = Template.instance().connectedKeywordHashCode.get();
+  var answerIsADescription = (contentIterations % 2) == 0;
+  answerObj = {
+    answer: usersAnswer,
+    isADescriptionAnswer: answerIsADescription,
+    hashCode: keywordHashCode,
+  }
+  return answerObj;
+}
+
+
 function changeFlashcardText(){
+  checkIfContentHasBeenShowedFourTimes();
   setFlashcardText(currentIndex += 1);
+
 }
-
-
 function setFlashcardText(index){
-  Template.instance().currentFlashcardText.set(testArray[index].keywordValueAnswer);
-  Template.instance().currentHiddenFlashcardText.set( testArray[index].keywordDescriptionAnswer);
+  let value = testArray[index].keywordValue;
+  let description = testArray[index].keywordDescription;
+  let connectedKeywordCode = testArray[index].connectedKeywordHashCode;
+
+  if((contentIterations % 2) != 0){
+    // The user sees a keywordDescription and answers with a keywordValue
+    Template.instance().currentFlashcardText.set(description);
+    Template.instance().currentHiddenFlashcardText.set(value);
+    Template.instance().connectedKeywordHashCode.set(connectedKeywordCode);
+  }else{
+    // The user sees a keywordValue and answers with a keywordDescription
+    Template.instance().currentFlashcardText.set(value);
+    Template.instance().currentHiddenFlashcardText.set(description);
+    Template.instance().connectedKeywordHashCode.set(connectedKeywordCode);
+  }
 }
+
+
+
+Template.flashcardPracticePage.onCreated(function() {
+  let template = Template.instance();
+  subscriptions(template);
+
+  this.currentFlashcardText = new ReactiveVar();
+  this.currentHiddenFlashcardText = new ReactiveVar();
+  this.connectedKeywordHashCode = new ReactiveVar();
+
+  setFlashcardText(currentIndex);
+});
 
 Template.flashcardPracticePage.helpers({
   flashcardText: function(){
@@ -43,16 +128,6 @@ Template.flashcardPracticePage.helpers({
   hiddenFlashcardText: function(){
     return  Template.instance().currentHiddenFlashcardText.get();
   },
-});
-
-Template.flashcardPracticePage.onCreated(function() {
-  let template = Template.instance();
-  subscriptions(template);
-
-  this.currentFlashcardText = new ReactiveVar();
-  this.currentHiddenFlashcardText = new ReactiveVar();
-
-  setFlashcardText(currentIndex);
 });
 
 Template.flashcardPracticePage.onRendered(function(){
@@ -86,21 +161,10 @@ Template.flashcardPracticePage.events({
 
   "click #submit-flashcard-answer-btn": function(event) {
     event.preventDefault();
+    answer = createAnswerObject();
+    arrayOfAnswers.push(answer);
+    // TODO clear form text
     changeFlashcardText();
   },
-
-  'change select': function( event, template ) {
-    if ( $( event.target ).val() === "show" ) {
-      // Here we get our template instance from the template
-      // argument in our event handler.
-      template.showExtraFields.set( true );
-    } else {
-      template.showExtraFields.set( false );
-    }
-  }
-
-
-
-
 
 });
